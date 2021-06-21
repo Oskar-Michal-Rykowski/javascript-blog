f8373ae500ba025b785e3a1d1f9b5780396eccac
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-06-22 00:59:28 +0200

    Add all necessary functions mentioned in Kodillad tasks

--- js/script.js
+++ js/script.js
@@ -15,11 +15,10 @@ const titleClickHandler = function (event) {
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
-    clickedElement.classList.add('active');
   }
 
   /* [IN PROGRESS] add class 'active' to the clicked link */
-
+  clickedElement.classList.add('active');
   console.log('clickedElement', clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
@@ -32,9 +31,17 @@ const titleClickHandler = function (event) {
 
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('articleSelector :', articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('targetArticle :', targetArticle);
+
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
 };
 
 const links = document.querySelectorAll('.titles a');
