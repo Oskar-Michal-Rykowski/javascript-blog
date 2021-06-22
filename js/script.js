0b8cd9a4ef4779d4aa202251c518e07d9a2b7ddc
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-06-23 01:03:06 +0200

    Add generateTitleLinks with innerHTML

--- js/script.js
+++ js/script.js
@@ -32,3 +32,48 @@ const links = document.querySelectorAll('.titles a');
 for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+optTitleSelector = '.post-title',
+optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+/* for each article */
+
+const articles = document.querySelectorAll(optArticleSelector);
+for (article of articles) {
+
+
+
+
+/* get the article id */
+
+const articleId = article.getAttribute('id')
+
+
+  /* find the title element */
+
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+  /* get the title from the title element */
+
+  /* create HTML of the link */
+
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+console.log('linkHTML' + linkHTML)
+  /* insert link into titleList */
+
+  titleList.innerHTML = titleList.innerHTML + linkHTML;}
+}
+
+
+
+generateTitleLinks();
+  
+
